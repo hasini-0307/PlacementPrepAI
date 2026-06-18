0682ad9b8ffbd7ffd3cdc7a69068161e095c5c1f
Kolasani Hasini <hasini.kolasani@gmail.com>
2026-06-18 17:13:13 +0530

    Add MultiQuery Retriever for improved document retrieval

--- src/retriever.py
+++ src/retriever.py
@@ -1,10 +1,25 @@
+from langchain_classic.retrievers.multi_query import MultiQueryRetriever
+
+from src.llm import get_llm
+
+
 def get_retriever(vectorstore):
 
-    retriever = vectorstore.as_retriever(
+    # Base retriever
+    base_retriever = vectorstore.as_retriever(
         search_type="mmr",
         search_kwargs={
             "k": 3
         }
+    )
+
+    # LLM for generating multiple queries
+    llm = get_llm()
+
+    # MultiQuery Retriever
+    retriever = MultiQueryRetriever.from_llm(
+        retriever=base_retriever,
+        llm=llm
     )
 
     return retriever
